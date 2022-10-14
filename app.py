34930a87e650f4bca99b5e0ec87569d41973c83f
Brandon Chen <bc2982@nyu.edu>
2022-10-14 14:36:03 -0400

    create the skeleton of the website with register form

--- app.py
+++ app.py
@@ -31,12 +31,31 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     """
     Route for the register page
     """
-    return render_template("register.html")
+    if (request.args):
+        # add valid username and login checking here
+        # current only has a placeholder
+        if (bool(request.args["username"]) and bool(request.args["password"]) and bool(request.args["match"])):
+            userInput = request.args["username"]
+            passwordInput = request.args["password"]
+            if (bool[request.args["username"]]): # PLACEHOLDER - Check that the username hasnt been used
+                if (request.args["password"] == request.args["match"]):
+                    return(redirect(url_for("homepage")))
+                else:
+                    flash('Password must match.')
+                    return(redirect(url_for("register")))
+            else:
+                flash('Username has been taken.')
+                return(redirect(url_for("register")))
+        else:
+            flash('Please enter all fields.')
+            return(redirect(url_for("register")))
+    else:
+        return render_template("register.html")
 
 @app.route('/homepage')
 def homepage():
@@ -44,6 +63,27 @@ def homepage():
     Route for the homepage page
     """
     return render_template("homepage.html")
+
+@app.route('/all')
+def all():
+    """
+    Route for the view all page
+    """
+    return render_template("all.html")
+
+@app.route('/add')
+def add():
+    """
+    Route for the add todo page
+    """
+    return render_template("add.html")
+
+@app.route('/logout')
+def logout():
+    """
+    Route to logout
+    """
+    return(redirect(url_for("login")))
 
 if __name__ == "__main__":
     app.run(debug = True)
